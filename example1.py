89677e797801b1bf51095d4d234a9e6d03f9001a
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2023-12-08 16:02:56 +0330

    Create example1.py

---
+++ example1.py
@@ -0,0 +1,5 @@
+
+width = float(input("please enter the width of the rectangle: ")) 
+length = float(input("please enter the length of the rectangle: "))
+Area = width * length
+print("Area of rectangle = " + str(Area))
